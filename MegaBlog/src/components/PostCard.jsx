f9fcffa728937af56621ea47fc9063ecf1141a45
awarkopyr <awarkohm2002@gmail.com>
2024-08-03 20:45:51 +0600

    css fixed

--- MegaBlog/src/components/PostCard.jsx
+++ MegaBlog/src/components/PostCard.jsx
@@ -4,14 +4,17 @@ import { Link } from 'react-router-dom'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function PostCard({ $id, title, featuredImage }) {
+function PostCard({ $id, title, featuredImage, content }) {
 
     return (
         <Link to={`/post/${$id}`}>
             <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" src={appwriteService.getFilePreview(featuredImage)} alt={title} />
                 <Card.Body>
-                    <Card.Title>{ title }</Card.Title>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>
+                        {content}
+                    </Card.Text>
                     <Button variant="primary">Click to read</Button>
                 </Card.Body>
 
