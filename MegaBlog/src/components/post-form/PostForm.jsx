274885380b8dac2ce831981aab69fc5e23c07ab9
awarkopyr <awarkohm2002@gmail.com>
2024-08-03 22:23:15 +0600

    postform

--- MegaBlog/src/components/post-form/PostForm.jsx
+++ MegaBlog/src/components/post-form/PostForm.jsx
@@ -90,7 +90,7 @@ export default function PostForm({ post }) {
                 />
                 <RTE label="Content :" name="content" control={control} defaultValue={getValues("content")} />
             </div>
-            <div className="w-1/3 px-2">
+            <div className="w-1/1 px-2">
                 <Input
                     label="Featured Image :"
                     type="file"
